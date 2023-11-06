e47bd683b8c3484a88cc5e4c3f034376465d6ca1
madki <achmadzaki113@gmail.com>
2023-11-06 13:09:47 +0700

    benerin error gk jlas

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     public function proseslogin(Request $request){
-        if(Auth::guard('karyawan')->attempt(['nik'=>$request->nik,'password'=>$request->password])) {
-            echo "berhasil login";
+        if(Auth::guard('karyawan')->attempt(['nik' => $request->nik,'password' => $request->password])) {
+            return redirect('/dashboard');
         }else{
             echo "gagal login";
         }
