984afd302930246a51e9c6835305c4d1f0ddaba0
madki <achmadzaki113@gmail.com>
2023-11-14 21:05:33 +0700

    eps 7, menit ke 30.00

--- database/presensigps.sql
+++ database/presensigps.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Waktu pembuatan: 08 Nov 2023 pada 01.46
+-- Waktu pembuatan: 14 Nov 2023 pada 14.04
 -- Versi server: 8.0.30
 -- Versi PHP: 8.1.10
 
@@ -54,12 +54,19 @@ CREATE TABLE `presensi` (
   `nik` char(5) COLLATE utf8mb4_general_ci NOT NULL,
   `tgl_presensi` date NOT NULL,
   `jam_in` time NOT NULL,
-  `jam_out` time NOT NULL,
+  `jam_out` time DEFAULT NULL,
   `foto_in` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `foto_out` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `location_in` text COLLATE utf8mb4_general_ci NOT NULL,
-  `location_out` text COLLATE utf8mb4_general_ci NOT NULL
+  `foto_out` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `lokasi_in` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `lokasi_out` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `presensi`
+--
+
+INSERT INTO `presensi` (`id`, `nik`, `tgl_presensi`, `jam_in`, `jam_out`, `foto_in`, `foto_out`, `lokasi_in`, `lokasi_out`) VALUES
+(NULL, '12345', '2023-11-14', '14:02:24', NULL, '12345-2023-11-14.png', NULL, '-6.2193386,106.8384621', NULL);
 
 -- --------------------------------------------------------
 
